9ccf64b879cfe03db0b9caed94e5cdb5bea3d030
AuRLabLeuven <AuRLabLeuven@gmail.com>
2019-07-10 16:07:48 +0200

    Fix THR Curve
    by Eric

--- adaptive/spikes/THRcurve.m
+++ adaptive/spikes/THRcurve.m
@@ -252,7 +252,7 @@ function BlockIndices = local_block_index_per_spike(begin_block_times, SpikeTime
     BlockIndices = [];
     if ~isempty(SpikeTimes)
         for i = 1:length(SpikeTimes)
-            BlockIndices = zeros(1,length(SpikeTimes));
+%             BlockIndices = zeros(1,length(SpikeTimes));
            SpikeTime = SpikeTimes(i);
            BlockIndices(i) = sum(begin_block_times < SpikeTime);
         end
